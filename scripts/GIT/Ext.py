77399bc4f93ab2b32d34b4e493879c6648b04c74
qve1t <warpas.piotr@gmail.com>
2020-05-16 15:05:29 +0200

    Added new git commands

--- scripts/GIT/Ext.py
+++ scripts/GIT/Ext.py
@@ -7,16 +7,33 @@ class Ext:
 	def __init__(self, ownerComp):
 		# The component to which this extension is attached
 		self.ownerComp = ownerComp
+		self.gitCommandReplace = "MESSAGE"
 		self.cmdOutput = op('cmd_output')
+		self.gitCommands = op('git_commands')
 
 	def RunCommand(self, command):
 		#command should be a list of strings
-		process = subprocess.Popen(command, stdout=subprocess.PIPE)
+		commandList = str(command).split(" ")
+		if self.gitCommandReplace in commandList:
+			commandList[commandList.index("MESSAGE")] = str(op('message')[0,0])
+
+		process = subprocess.Popen(commandList, stdout=subprocess.PIPE)
+		
 		return process.communicate()[0]
 
 	def PrintOutput(self, message):
-		self.cmdOutput.text = message.decode() 
+		self.cmdOutput.text = self.MessageDecode(message)
 
-	def CommandGitStatus(self):
-		command = ["git", 'status']
-		self.PrintOutput(self.RunCommand(command))
+	def MessageDecode(self, message):
+		try:
+			return message.decode()
+		except:
+			return message 
+
+	def RunGitCommand(self, command):
+		commandToRun = self.gitCommands[command, 'command']
+		if commandToRun == None:
+			self.PrintOutput("Wrong command")
+			return
+
+		self.PrintOutput(self.RunCommand(commandToRun))
