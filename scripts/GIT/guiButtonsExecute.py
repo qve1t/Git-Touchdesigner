4be503b89a324747cca36c4ff8b0ae1d556645cd
qve1t <warpas.piotr@gmail.com>
2020-05-17 14:09:22 +0200

    Added branches and checkout command

--- scripts/GIT/guiButtonsExecute.py
+++ scripts/GIT/guiButtonsExecute.py
@@ -2,12 +2,17 @@
 def onOffToOn(channel, sampleIndex, val, prev):
 	if channel.name == "clean":
 		op.GIT.OutputClean()
-	else:
+		return
+
+	elif op.GIT.op('git_commands')[channel.name, 'command'] != None:
 		if channel.name == "commit" and op('message_null')[0,0] == "":
 			op.GIT.PrintOutput("Write commit message")
 			return
-
 		op.GIT.RunGitCommand(channel.name)
+		return
+
+	op.GIT.PrintOutput("Comand Error")
+
 
 def whileOn(channel, sampleIndex, val, prev):
 	return
