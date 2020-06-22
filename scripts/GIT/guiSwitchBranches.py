e4318b656af90953c29463514b426dc3993425cf
qve1t <warpas.piotr@gmail.com>
2020-06-22 22:00:28 +0200

    quick function fix

--- scripts/GIT/guiSwitchBranches.py
+++ scripts/GIT/guiSwitchBranches.py
@@ -1,4 +1,4 @@
 def onOffToOn(channel, sampleIndex, val, prev):
 	commandToRun = op.GIT.gitCommands['checkout', 'command']
-    op.GIT.RunCommand(commandToRun, channel.name)
+    op.GIT.PrintOutput(op.GIT.RunCommand(commandToRun, channel.name))
     #przetestować jak github znowu zacznie dzialac
