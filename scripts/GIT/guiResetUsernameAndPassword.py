0c911b7c12b0e9d92e9649ad444e4ab9ca4595d3
qve1t <warpas.piotr@gmail.com>
2020-06-25 16:17:38 +0200

    quick fix

--- scripts/GIT/guiResetUsernameAndPassword.py
+++ scripts/GIT/guiResetUsernameAndPassword.py
@@ -1,10 +1,10 @@
 
 def onStart():
 	op('username/string')[0,0] = ''
-    op('password/string')[0,0] = ''
+	op('password/string')[0,0] = ''
 
 def onExit():
 	op('username/string')[0,0] = ''
-    op('password/string')[0,0] = ''
+	op('password/string')[0,0] = ''
 
 	
